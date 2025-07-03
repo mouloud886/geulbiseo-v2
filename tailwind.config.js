fd57de62fd2f6cc14f07704364827f2977ea372c
mouloud886 <mouloud886@gmail.com>
2025-07-03 14:30:22 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   prefix: "",
   theme: {
     container: {
