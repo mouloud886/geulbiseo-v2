92f79b902d3a69d944ea6f330dce50ec1d70ab6d
mouloud886 <mouloud886@gmail.com>
2025-07-03 20:08:11 +0900

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,2 @@
-// craco.config.js (루트에 있는 파일을 이걸로 완전 교체)
-const { POSTCSS_MODES } = require('@craco/craco');
-
-module.exports = {
-  style: {
-    postcss: {
-      // 반드시 file 모드만 지정, plugins 배열은 postcss.config.js에서 관리
-      mode: POSTCSS_MODES.file,
-    },
-  },
-};
+// craco.config.js (루트에 있는 파일을 이 내용으로 완전 교체)
+module.exports = {};
